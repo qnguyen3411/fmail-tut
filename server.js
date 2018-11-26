7e42d454551693c5716e90471e6c48b3efe91e76
Quang Nguyen <qnguyen3411@gmail.com>
2018-11-26 15:25:40 -0800

    implemented header

---
+++ server.js
@@ -0,0 +1,5 @@
+const app = require('./src/app');
+
+app.listen(8000, () => {
+  console.log("LISTENING ON 8000")
+});
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, "src", "public", "js", "main.jsx"),
+  output: {
+    path: path.join(__dirname, "dist"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".jsx", ".js"]
+  },
+
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: ['babel-preset-react']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "views", "index.html")
+    })
+  ]
+}
